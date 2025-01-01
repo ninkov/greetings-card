47e346672dd75357647bf6281d1fdb0095a3869b
Ninkov <ninkov82@gmail.com>
2025-01-02 00:00:00 +0200

    issue fix

--- src/components/PhotoUpload.jsx
+++ src/components/PhotoUpload.jsx
@@ -1,12 +1,9 @@
-import React from "react";
-
 export default function PhotoUpload({ setPhoto }) {
   const handlePhotoUpload = (ev) => {
-    const file = ev.target.file[0];
+    const file = ev.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = (e) => setPhoto(e.target.result);
-      console.log(e);
       reader.readAsDataURL(file);
     }
   };
