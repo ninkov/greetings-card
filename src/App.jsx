ba8a3e0e67f9a0fd691e48b98a65d7371b2dfc69
Ninkov <ninkov82@gmail.com>
2025-01-02 08:23:24 +0200

    display an image and includes a text area

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 
 import './App.css'
 import PhotoUpload from './components/PhotoUpload'
+import CardView from './components/CardView'
 
 function App() {
   const [photo, setPhoto] = useState(null)
+  const [text, setText] = useState('')
 
   return (
     <>
-      <PhotoUpload/>
+      <PhotoUpload setPhoto={setPhoto}/>
+      <CardView text={text} setText={setText} photo={photo} />
     </>
   )
 }
---
+++ src/components/CardView.jsx
@@ -0,0 +1,20 @@
+
+
+export default function CardView({photo,text,setText}) {
+  return (
+    <div className="card">
+
+{photo&&(
+    <div className="frame">
+        <img src={photo} alt="upload" className="photo"/>
+    </div>
+    )}
+<textarea 
+value={text}
+onChange={(e)=>setText(e.target.value)}
+className="text"
+></textarea>
+
+    </div>
+  )
+}
