4752516454f4ed39cdf6bafa30fd4e396ae5e26c
Ninkov <ninkov82@gmail.com>
2025-01-10 19:39:37 +0200

    Fix errors CSP

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: "/invitation-card",
   plugins: [react()],
-})
+  server: {
+    headers: {
+      // Add relaxed CSP for development
+      'Content-Security-Policy': mode === 'development'
+        ? "script-src 'self' 'unsafe-inline' 'unsafe-eval';"
+        : "script-src 'self';", // Strict CSP for production
+    },
+  },
+  build: {
+    // Specify output directory for production builds
+    outDir: 'dist',
+  },
+}));
